569be150610d09eacb257bbd130968468ba65386
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-19 11:42:36 +0500

    5-misol

--- sum_even.py
+++ sum_even.py
@@ -1,7 +1,12 @@
-#A four-digit integer is given. Find the sum of even digits in it.
-
-#Create a variable "var_int" and assign it a four-digit integer value.
-
-#Create a variable "sum_even" and assign it 0.
-
-#Find the sum of the even digits in the variable "var_int".
+var_int = 8548
+sum_even = 0
+var_int1 = var_int // 1000
+var_int1 *= (var_int1 + 1) % 2
+var_int2 = var_int // 100 % 10
+var_int2 *= (var_int2 + 1) % 2
+var_int3 = var_int // 10 % 10
+var_int3 *= (var_int3 + 1) % 2
+var_int4 = var_int // 1 % 10
+var_int4 *= (var_int4 + 1) % 2
+sum_even = var_int1 + var_int2 + var_int3 + var_int4
+print(sum_even)
