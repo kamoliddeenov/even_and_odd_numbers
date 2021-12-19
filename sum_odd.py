e10128d79f99c6f6f526ec93da2c6b8c1decf88b
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-19 11:44:34 +0500

    6-misol

--- sum_odd.py
+++ sum_odd.py
@@ -1,7 +1,12 @@
-#A four-digit integer is given. Find the sum of odd digits in it.
-
-#Create a variable "var_int" and assign it a four-digit integer value.
-
-#Create a variable "sum_even" and assign it 0.
-
-#Find the sum of the odd digits in the variable "var_int".
+var_int = 7439
+sum_even = 0
+var_int1 = var_int // 1000
+var_int1 *= var_int1% 2
+var_int2 = var_int // 100 % 10
+var_int2 *= var_int2 % 2
+var_int3 = var_int // 10 % 10
+var_int3 *= var_int3 % 2
+var_int4 = var_int // 1 % 10
+var_int4 *= var_int4 % 2
+sum_even = var_int1 + var_int2 + var_int3 + var_int4
+print(sum_even)
